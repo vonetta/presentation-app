30b0ee3157d33c8f9295ed7fd79b98e78521b4f6
Vonetta <vonettastevenson@gmail.com>
2019-09-26 16:36:44 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Slide from "./Slide";
+import "../App.css";
+function App() {
+  return (
+    <>
+      <p className="title ">Presentation App</p>
+      <Slide />
+    </>
+  );
+}
+
+export default App;
